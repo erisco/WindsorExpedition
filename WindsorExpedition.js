ba35f84ec6482de1f0ff7a57ea3a98851bb515e4
Adam Heinermann <heinerm@uwindsor.ca>
2013-01-20 07:27:50 -0500

    Halve the icon size of transit/heritage

--- WindsorExpedition.js
+++ WindsorExpedition.js
@@ -124,7 +124,15 @@ WindsorExpedition.prototype.rebuildOverlay = function () {
                                               bufferBounds.getSouthWest().lng(),
                                               bufferBounds.getNorthEast().lat(),
                                               bufferBounds.getNorthEast().lng() );
+    var pxSize = this.__overlay.getIconPxSize();
     for ( type in places ) {
+      // Make more common icons smaller than less common ones
+      if ( type == "transit" || type == "heritage" )
+        this.__overlay.setIconPxSize(pxSize / 2.0);
+      else
+        this.__overlay.setIconPxSize(pxSize);
+        
+      // Iterate each object of the current type and draw the image if it hasn't been found
       var imgType = type + ".png";
       for ( spot in places[type] ) {
         if ( !places[type][spot].found /*&& type != "transit" && type != "heritage"*/ )
